17df51c25e3db842ff552b0845e3b8817a107bd3
mingfei <mingfei.net@gmail.com>
2017-05-15 17:29:36 +0800

    Update

--- sql/day1.sql
+++ sql/day1.sql
@@ -1,4 +1,25 @@
 -- dialect ['daɪəlekt] n. 方言；地方话
 -- RDBMS SQL
 
-SHOW DATABASES ;
+SHOW DATABASES; -- Ctrl(Command) + Enter
+
+CREATE DATABASE db_1702;
+
+DROP DATABASE db_1702;
+
+USE db_1702;
+
+SHOW TABLES;
+
+CREATE TABLE t_student (
+  sno    VARCHAR(10), -- variable character 可变长字符类型
+  sname  VARCHAR(10),
+  gender CHAR(1),
+  age    INT(2)
+);
+
+SELECT *
+FROM t_student;
+
+INSERT INTO t_student
+VALUES ('2017001', '张三', '男', 18);
