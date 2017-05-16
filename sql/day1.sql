3ca9e790fbaa1d6c5f418cb336ddc39d3de5a714
mingfei <mingfei.net@gmail.com>
2017-05-16 09:14:22 +0800

    Update

--- sql/day1.sql
+++ sql/day1.sql
@@ -7,21 +7,25 @@ CREATE DATABASE db_1702;
 
 DROP DATABASE db_1702;
 
-USE db_1702;
+-- USE db_1702;
 
 SHOW TABLES;
 
-CREATE TABLE t_student (
+CREATE TABLE db_1702.t_student (
   sno    VARCHAR(10), -- variable character 可变长字符类型
-  sname  VARCHAR(10),
+  sname  VARCHAR(10), -- 属性 列 字段 column
   gender CHAR(1),
   age    INT(2)
 );
-
-DROP TABLE t_student;
+DROP TABLE db_1702.t_student;
 
 SELECT *
-FROM t_student;
+FROM db_1702.t_student;
 
-INSERT INTO t_student
+INSERT INTO db_1702.t_student
 VALUES ('2017001', '张三', '男', 18);
+
+-- 1PB = 1024 TB
+
+SELECT 1 + 1;
+
